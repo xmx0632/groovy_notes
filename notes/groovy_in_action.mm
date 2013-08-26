acac8660d4d5b4be5161b3bb13dbe29deb946252
xmx0632 <xmx0632@gmail.com>
2013-08-26 16:53:49 +0800

    add

--- notes/groovy_in_action.mm
+++ notes/groovy_in_action.mm
@@ -6,5 +6,38 @@
 <node TEXT="groovyc &#x2013;d classes Fibonacci.groovy" ID="ID_206841731" CREATED="1377395681306" MODIFIED="1377395684065"/>
 <node TEXT="java -cp %GROOVY_HOME%/embeddable/groovy-all-2.1.6.jar;classes Fibonacci" ID="ID_1269463136" CREATED="1377395627596" MODIFIED="1377395644785"/>
 </node>
+<node TEXT="2.4.3 The Groovy lifecycle" POSITION="right" ID="ID_1759722734" CREATED="1377497891562" MODIFIED="1377497905921">
+<node ID="ID_1317438844" CREATED="1377497853625" MODIFIED="1377497957234">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20219;&#20309;&#19996;&#35199;&#37117;&#26159;&#23545;&#35937;&#65292;&#25152;&#26377;&#25805;&#20316;&#37117;&#26159;&#26041;&#27861;&#35843;&#29992;
+    </p>
+    <p>
+      Groovy&#8217;s approach of treating every-thing as an object and all operators as method&#160;&#160;calls.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node POSITION="right" ID="ID_537051210" CREATED="1377504803000" MODIFIED="1377504890375">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.3.1 Overview of overridable operators<br/>&#25805;&#20316;&#31526;&#37325;&#36733;&#65292;&#21508;&#31181;+ - * / [] &gt;&gt; ....&#37117;&#33021;&#34987;&#37325;&#36733;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </map>
---
+++ src/DateType.groovy
@@ -0,0 +1,4 @@
+assert 'ABCDE'.indexOf(67) == 2
+println('aac'.indexOf(97))
+println 56
+"sdfsfs".padLeft(3)
